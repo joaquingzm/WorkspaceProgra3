d8a0a7fddb168031eb378f7b60d021622afd2710
Joaquín Guzmán <joaquin.gzm234@gmail.com>
2024-06-02 02:54:10 -0300

    Comencé Ej7, terminé Dijkstra

---
+++ ProyectoProgra3/tp5_ej7/AlgoritmosDijkstraFloyd.java
@@ -0,0 +1,62 @@
+package tp5_ej7;
+
+import java.util.List;
+
+import tp5.ejercicio1.Edge;
+import tp5.ejercicio1.listaAdy.AdjListGraph;
+import tp5.ejercicio1.listaAdy.AdjListVertex;
+
+public class AlgoritmosDijkstraFloyd<T> {
+	
+	public int[] Dijkstra(AdjListGraph<T> grafo,T origen){
+		if(grafo!=null&&!grafo.isEmpty()) {
+			AdjListVertex<T> origenV = (AdjListVertex<T>) grafo.search(origen);
+			AdjListVertex<T> auxV;
+			boolean visitados[] = new boolean[grafo.getSize()];
+			int costos[] = new int[grafo.getSize()];
+			for(int i=0;i<grafo.getSize();i++) {
+				costos[i]=9999;
+			}
+			//Posiciones del vertice desde donde se viene
+			int[] puntosDePartida = new int[grafo.getSize()];
+			
+			costos[origenV.getPosition()]=0;
+			puntosDePartida[origenV.getPosition()]=0;
+			
+			while(faltanVisitar(visitados)) {
+				auxV = (AdjListVertex<T>) grafo.getVertices().get(posDelMenorCostoNoVisitado(costos,visitados));
+				visitados[auxV.getPosition()]=true;
+				for(Edge<T> e : auxV.getEdges()) {
+					if(!visitados[e.getTarget().getPosition()]) {
+						if(costos[auxV.getPosition()]+e.getWeight()<costos[e.getTarget().getPosition()]) {
+							costos[e.getTarget().getPosition()]=costos[auxV.getPosition()]+e.getWeight();
+							puntosDePartida[e.getTarget().getPosition()]=auxV.getPosition();
+						}
+					}
+				}
+			}
+			return costos;
+		}
+		
+		return null;
+	}
+	
+	public boolean faltanVisitar(boolean visitados[]) {
+		for(int i=0;i<visitados.length;i++) {
+			if(!visitados[i])return true;
+		}
+		return false;
+	}
+	
+	public int posDelMenorCostoNoVisitado(int costos[],boolean visitados[]) {
+		int minCosto=9999;
+		int minPos=0;
+		for(int i=0;i<costos.length;i++) {
+			if(!visitados[i]&&costos[i]<minCosto) {
+				minCosto=costos[i];
+				minPos=i;
+			}
+		}
+		return minPos;
+	}
+}
---
+++ ProyectoProgra3/tp5_ej7/testAlgortimosDijkstraFloyd.java
@@ -0,0 +1,55 @@
+package tp5_ej7;
+
+import tp5.ejercicio1.listaAdy.AdjListGraph;
+import tp5.ejercicio1.listaAdy.AdjListVertex;
+
+/*
+ * DUDA: No se supone que Dijsktra funciona para grafos sin ciclos?
+ * Entiendo que en este caso funcione porque en los 2 ciclos que hay
+ * el vertice desde donde se larga el algoritmo es parte y por lo tanto 
+ * está marcado como visitado, pero si el ciclo de produciera entre otros
+ * vertices sí daría error, no? */
+
+public class testAlgortimosDijkstraFloyd {
+
+	public static void main(String[] args) {
+		AdjListGraph<Integer> grafo = new AdjListGraph<Integer>();
+		crearGrafoDijkstra(grafo);
+		AlgoritmosDijkstraFloyd<Integer> algoritmos = new AlgoritmosDijkstraFloyd<Integer>();
+		int arr[] = algoritmos.Dijkstra(grafo, 1);
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" , ");
+		}
+		
+	}
+	
+	public static void crearGrafoDijkstra(AdjListGraph<Integer> grafo) {
+		
+		AdjListVertex<Integer> v1 = (AdjListVertex<Integer>) grafo.createVertex(1);
+		AdjListVertex<Integer> v2 = (AdjListVertex<Integer>) grafo.createVertex(2);
+		AdjListVertex<Integer> v3 = (AdjListVertex<Integer>) grafo.createVertex(3);
+		AdjListVertex<Integer> v4 = (AdjListVertex<Integer>) grafo.createVertex(4);
+		AdjListVertex<Integer> v5 = (AdjListVertex<Integer>) grafo.createVertex(5);
+		AdjListVertex<Integer> v6 = (AdjListVertex<Integer>) grafo.createVertex(6);
+		
+		grafo.connect(v1, v6,5);
+		grafo.connect(v1, v5,10);
+		grafo.connect(v1, v3,40);
+		
+		grafo.connect(v6, v5,10);
+		grafo.connect(v6, v2,20);
+		
+		
+		grafo.connect(v5, v4,20);
+		
+		//En el pdf v3 apunta tmb a v5, pero eso vuelve al grafo ciclico, así que no lo hago
+		grafo.connect(v3, v5,5);
+		grafo.connect(v3, v2,10);
+		
+		//En el pdf v2 apunta tmb a v4, pero eso vuelve al grafo ciclico, así que no lo hago
+		grafo.connect(v2, v4,5);
+		
+		grafo.connect(v4, v3,5);
+	}
+
+}
